5c1f7221da4afb59a894220ca681ebfe73a15165
victorlfdev <victorlf2002@gmail.com>
2026-03-27 23:28:47 -0300

    feat: add allowedDevOrigins configuration to next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+// next.config.js
+module.exports = {
+  allowedDevOrigins: ['100.120.169.33'],
+};
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
 
 export default nextConfig;
